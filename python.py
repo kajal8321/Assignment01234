11b938b38b90dcea52b910eb18edfc222bc10b11
kajal8321 <146552306+kajal8321@users.noreply.github.com>
2023-10-20 23:22:52 +0530

    Add files via upload

--- python.py
+++ python.py
@@ -1,7 +1,11 @@
-# write a python program that acccepts a word from the user and reversed it.
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) #declaring the tuple
+count_odd = 0
+count_even = 0
+for x in numbers:
+    if not x % 2:
+        count_even+=1
+    else:
+        count_odd+=1
+print("Number of even numbers :" ,count_even)
+print("Number of odd numbers :" ,count_odd)         
 
-Name=str(input("enter the name:"))
-temp=""
-for i in Name:
-    temp=i+temp
-print("reversed_name:" ,temp)    
